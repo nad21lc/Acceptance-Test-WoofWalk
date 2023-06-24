24d9f31055bc05f29e958befce4e7c68310a2b39
Fiorella <U202120418@upc.edu.pe>
2023-06-23 22:21:48 -0500

    Change Profile Picture

--- pagina-web/scriptPage.js
+++ pagina-web/scriptPage.js
@@ -3,3 +3,24 @@ function menuToggle(){
     const toggleMenu = document.querySelector('.menu');
     toggleMenu.classList.toggle('active');
 }
+
+// Obtener elementos del DOM
+var fileInput = document.getElementById('file-upload');
+var avatarImage = document.querySelector('.perfil-usuario-avatar img');
+
+// Escuchar el evento "change" en el input de tipo archivo
+fileInput.addEventListener('change', function(event) {
+  var file = event.target.files[0];
+
+  // Crear objeto FileReader
+  var reader = new FileReader();
+
+  // Definir la función de carga completada del FileReader
+  reader.onload = function() {
+    // Establecer la imagen seleccionada como la nueva fuente de la imagen del avatar
+    avatarImage.src = reader.result;
+  };
+
+  // Leer el contenido del archivo como una URL de datos
+  reader.readAsDataURL(file);
+});

ffa11e138cf3e8b8c64364e4bd3b4157b91bbdad
Nadia <nad21lc@gmail.com>
2023-06-24 19:47:27 -0500

    added rate service

--- pagina-web/scriptPage.js
+++ pagina-web/scriptPage.js
@@ -3,3 +3,11 @@ function menuToggle(){
     const toggleMenu = document.querySelector('.menu');
     toggleMenu.classList.toggle('active');
 }
+
+//confirmación para detener el servicio
+document.getElementById("trip-finish").addEventListener("click", function(event) {
+    var confirmacion = confirm("¿Estás seguro de que deseas finalizar el paseo?");
+    if (confirmacion == false) {
+      event.preventDefault(); // Evita que se siga el enlace si la confirmación es cancelada
+    }
+});
